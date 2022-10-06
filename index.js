63e13e33eb14c4bb49fa5894707efd624b1d84b8
Marnix Kok <marnix@xinsolutions.co.nz>
2022-10-07 01:03:53 +1300

    changes.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 const Transpile = require('./transpile.js');
 
-const [script, filename] = process.argv;
+const [cwd, script, filename] = process.argv;
 console.log("Processing: ", filename);
 Transpile.transpile(filename);
